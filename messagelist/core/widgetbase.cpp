540322f33f0b8db06ae23a761fed37960638c7df
Andre Heinecke <aheinecke@intevation.de>
2011-05-19 15:27:25 +0200

    Fix LastMessage Selection.
        The id of the storagmodel is not constant over the lifecycle
        of a storagemodel. Immediatly after something new is selected
        in the selectionmodel the id's change to the new selection.
        This caused the messagelist manager to save the last selected
        message not for the colletions that were selected
        but for the collections about to be selected.
        Since the messageId then did not exist in the newly selected
        collection the messageId of the lastselected message was discarded.
    
        BUG: 272417

--- messagelist/core/widgetbase.cpp
+++ messagelist/core/widgetbase.cpp
@@ -95,6 +95,7 @@ public:
 
   View *mView;
   QString mLastAggregationId;
+  QString mLastStorageModelId;
   QString mLastThemeId;
   KLineEdit * mSearchEdit;
   QTimer * mSearchTimer;
@@ -345,8 +346,8 @@ void Widget::setStorageModel( StorageModel * storageModel, PreSelectionMode preS
   {
     // Save the current selection
     MessageItem * lastSelectedMessageItem = d->mView->currentMessageItem( false );
-    Manager::instance()->savePreSelectedMessageForStorageModel(
-        d->mStorageModel,
+    Manager::instance()->savePreSelectedMessageForStorageModelId(
+        d->mLastStorageModelId,
         lastSelectedMessageItem ? lastSelectedMessageItem->uniqueId() : 0
       );
   }
@@ -371,6 +372,7 @@ void Widget::setStorageModel( StorageModel * storageModel, PreSelectionMode preS
   StorageModel * oldModel = d->mStorageModel;
 
   d->mStorageModel = storageModel;
+  d->mLastStorageModelId = storageModel->id();
 
   d->mView->setStorageModel( d->mStorageModel, preSelectionMode );
 
