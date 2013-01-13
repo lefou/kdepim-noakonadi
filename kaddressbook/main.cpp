8e1fe86fb9b7c5840da4bcbdce2e32667731acc6
Pali Rohár <pali.rohar@gmail.com>
2013-01-13 11:16:00 +0100

    Revert "Start kdepim 4.4 asynchronously if built against 4.6."
    This reverts commit cfa404b7188e4c26bddbc9579728f6d25f8cd214.

--- kaddressbook/main.cpp
+++ kaddressbook/main.cpp
@@ -49,13 +49,11 @@ int main( int argc, char **argv )
   MainWindow *window = new MainWindow;
   window->show();
 
-#if !KDE_IS_VERSION(4,6,0)
   if ( !Akonadi::Control::start( window ) ) {
     //TODO: add message box after string freeze
     kWarning() << "Unable to start Akonadi server, exit application";
     return 1;
   }
-#endif
 
   return app.exec();
 }
